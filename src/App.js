f53a5c647c2d840ea837e474e2bfa013e341d257
unknown <mk9006549346@gmail.com>
2023-10-16 16:00:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Signup from './component/Signup';
+import Login from './component/login';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Signup/>}/>
+              <Route path='/login' element={<Login/>}/>
+            </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
